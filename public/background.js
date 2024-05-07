f6cf369b3ad902497fe99396c7b83498cd108d30
go0p <gp827782797@qq.com>
2024-05-07 22:53:11 +0800

    add inject code

--- public/background.js
+++ public/background.js
@@ -8,41 +8,52 @@ const websocketManager = new WebSocketManager();
 
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     console.log("msg", msg)
-    if (msg.action === ActionType.CONNECT) {
-        console.info("Start to connect websocket")
-        const host = msg['host'] || "127.0.0.1"
-        const port = msg['port'] || 11212
-        websocketManager.connectWebsocket(`ws://${host}:${port}/?token=${"a"}`)
-    } else if (msg.action === ActionType.DISCONNECT) {
-        websocketManager.disconnectWebsocket()
-    } else if (msg.action === ActionType.SETPROXY) {
-        chrome.proxy.settings.set({
-            value: {
-                mode: "fixed_servers",
-                rules: {
-                    singleProxy: {
-                        scheme: msg.scheme,
-                        host: msg.host,
-                        port: parseInt(`${msg.port}`)
+    switch (msg.action) {
+        case ActionType.CONNECT:
+            console.info("Start to connect websocket")
+            const host = msg['host'] || "127.0.0.1"
+            const port = msg['port'] || 11212
+            websocketManager.connectWebsocket(`ws://${host}:${port}/?token=${"a"}`)
+            break;
+        case ActionType.DISCONNECT:
+            websocketManager.disconnectWebsocket();
+            break;
+        case ActionType.SETPROXY:
+            chrome.proxy.settings.set({
+                value: {
+                    mode: "fixed_servers",
+                    rules: {
+                        singleProxy: {
+                            scheme: msg.scheme,
+                            host: msg.host,
+                            port: parseInt(`${msg.port}`)
+                        }
                     }
+                },
+                scope: 'regular',
+            });
+            break;
+        case ActionType.CLEARPROXY:
+            chrome.proxy.settings.clear({})
+            break;
+        case ActionType.PROXYSTATUS:
+            chrome.proxy.settings.get({}, function (details) {
+                if (details.value && details.value.mode === "fixed_servers") {
+                    let proxyConfig = details.value.rules.singleProxy;
+                    chrome.runtime.sendMessage({
+                        enable: true,
+                        proxy: `${proxyConfig.scheme}://${proxyConfig.host}:${proxyConfig.port}`
+                    })
+                } else {
+                    chrome.runtime.sendMessage({enable: false, proxy: ""})
                 }
-            },
-            scope: 'regular',
-        })
-
-    } else if (msg.action === ActionType.CLEARPROXY) {
-        chrome.proxy.settings.clear({})
-    } else if (msg.action === ActionType.PROXYSTATUS) {
-        chrome.proxy.settings.get({}, function (details) {
-            if (details.value && details.value.mode === "fixed_servers") {
-                let proxyConfig = details.value.rules.singleProxy;
-                chrome.runtime.sendMessage({
-                    enable: true,
-                    proxy: `${proxyConfig.scheme}://${proxyConfig.host}:${proxyConfig.port}`
-                })
-            } else {
-                chrome.runtime.sendMessage({enable: false, proxy: ""})
-            }
-        });
+            });
+            break;
     }
 })
+
+chrome.tabs.query({}, (tabs) => {
+    tabs.forEach(function (tab) {
+        console.log("Tab ID",tab.id); // 输出每个标签页的ID
+    });
+});
