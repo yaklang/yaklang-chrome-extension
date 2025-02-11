bb822b2d5b29cbda05b65dae716e291da684d53b
go0p <gp827782797@qq.com>
2025-02-11 21:43:40 +0800

    fix ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import "./App.css";
+// import "./App.css";
 import {ConfigProvider} from "antd";
 import {Contro} from "@components/Contro";
 import {Proxifier} from "@components/Proxifier";
 import {EvalInTab} from "@components/EvalInTab";
 import {ProxySwitch} from "@components/ProxySwitch";
+import './styles/global.css';
 
 function App() {
     return (
