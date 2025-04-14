4be4ab9101dfd5f37efcc24fb518832df14a7b8b
go0p <gp827782797@qq.com>
2025-04-14 17:20:53 +0800

    fix popup ui

--- src/entrypoints/options/main.tsx
+++ src/entrypoints/options/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import App from './App';
+import '@/styles/global.css'
 import './style.css';
 
 const root = createRoot(document.getElementById('app')!);
-root.render(<App />); 
+root.render(<App/>);
